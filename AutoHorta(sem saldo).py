df10a464be895977c916443af4d412e117f431ec
RVAlves <82850205+RVAlves@users.noreply.github.com>
2021-04-24 06:15:09 -0300

    Update AutoHorta(sem saldo).py

--- AutoHorta(sem saldo).py
+++ AutoHorta(sem saldo).py
@@ -1,53 +1,97 @@
 import numpy as np
-from numpy import array
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
-import time
 
-umid = ctrl.Antecedent(np.arange(0, 101, 1), 'umid')
+# Cria as variáveis do problema
+temp = ctrl.Antecedent(np.arange(0, 41, 1), 'temp')
+u_ar = ctrl.Antecedent(np.arange(0, 101, 1), 'u_ar')
+u_solo = ctrl.Antecedent(np.arange(0, 1025, 1), 'u_solo')
 
-umid.universe
-array([  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,
-13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,
-26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,
-39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,
-52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,  64,
-65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,
-78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,
-91,  92,  93,  94,  95,  96,  97,  98,  99, 100])
-
-umid
-Antecedent: umid
-
-temp = ctrl.Antecedent(np.arange(0, 51, 1), 'temp')
 irrig = ctrl.Consequent(np.arange(30,101,30), 'irrig')
 
-umid.automf(number=3, names=['Seco', 'Médio', 'Úmido'])
-temp ['Frio'] = fuzz.trimf(temp.universe, [0,0,18])
-temp ['Agradável'] = fuzz.trimf(temp.universe, [18,24,50])
-temp ['Quente'] = fuzz.trimf(temp.universe, [24,50,50])
-umid.view()
-temp.view()
+# Cria manualmente o mapeamento entre valores nítidos e difusos
+temp ['frio'] = fuzz.trimf(temp.universe, [0,0,20])
+temp ['agradavel'] = fuzz.trapmf(temp.universe, [10,21,22,35])
+temp ['quente'] = fuzz.trimf(temp.universe, [22,40,40])
 
-irrig ['ligada'] = fuzz.trimf(irrig.universe, [30, 30, 60])
+u_ar ['seco'] = fuzz.trapmf(u_ar.universe, [0, 0, 35, 60])
+u_ar ['ideal'] = fuzz.trapmf(u_ar.universe, [35, 60, 65, 80])
+u_ar ['umido'] = fuzz.trimf(u_ar.universe, [65, 100, 100])
+
+u_solo ['seco'] = fuzz.gaussmf(u_solo.universe, 0, 256)
+u_solo ['medio'] = fuzz.gaussmf(u_solo.universe, 512, 256)
+u_solo ['umido'] = fuzz.gaussmf(u_solo.universe, 1024, 256)
+
+irrig ['ligar'] = fuzz.trimf(irrig.universe, [30, 30, 60])
 irrig ['manter'] = fuzz.trimf(irrig.universe, [30, 60, 90])
-irrig ['desligada'] = fuzz.trimf(irrig.universe, [60, 90, 90])
+irrig ['desligar'] = fuzz.trimf(irrig.universe, [60, 90, 90])
 
-regra1 = ctrl.Rule(temp['Frio'] & umid['Seco'], irrig['ligada'])
-regra2 = ctrl.Rule(temp['Frio'] & umid['Médio'], irrig['manter'])
-regra3 = ctrl.Rule(temp['Frio'] & umid['Úmido'], irrig['desligada'])
-regra4 = ctrl.Rule(temp['Agradável'] & umid['Seco'], irrig['ligada'])
-regra5 = ctrl.Rule(temp['Agradável'] & umid['Médio'], irrig['manter'])
-regra6 = ctrl.Rule(temp['Agradável'] & umid['Úmido'], irrig['desligada'])
-regra7 = ctrl.Rule(temp['Quente'] & umid['Seco'], irrig['ligada'])
-regra8 = ctrl.Rule(temp['Quente'] & umid['Médio'], irrig['ligada'])
-regra9 = ctrl.Rule(temp['Quente'] & umid['Úmido'], irrig['desligada'])
+temp.view()
+u_ar.view()
+u_solo.view()
 
-sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3, regra4, regra5, regra6, regra7, regra8, regra9])
+irrig.view()
+
+# Cria as regras de decisão difusa
+
+# Para temperatura (frio)
+rule1 = ctrl.Rule(temp['frio'] & u_ar['seco'] & u_solo['seco'], irrig['ligar'])
+rule2 = ctrl.Rule(temp['frio'] & u_ar['seco'] & u_solo['medio'], irrig['ligar'])
+rule3 = ctrl.Rule(temp['frio'] & u_ar['seco'] & u_solo['umido'], irrig['desligar']) # Verificar
+
+
+rule4 = ctrl.Rule(temp['frio'] & u_ar['ideal'] & u_solo['seco'], irrig['ligar'])
+rule5 = ctrl.Rule(temp['frio'] & u_ar['ideal'] & u_solo['medio'], irrig['ligar'])
+rule6 = ctrl.Rule(temp['frio'] & u_ar['ideal'] & u_solo['umido'], irrig['desligar'])
+
+
+rule7 = ctrl.Rule(temp['frio'] & u_ar['umido'] & u_solo['seco'], irrig['ligar'])
+rule8 = ctrl.Rule(temp['frio'] & u_ar['umido'] & u_solo['medio'], irrig['ligar'])
+rule9 = ctrl.Rule(temp['frio'] & u_ar['umido'] & u_solo['umido'], irrig['desligar'])
+
+# Para temperatura (agradavel)
+rule10 = ctrl.Rule(temp['agradavel'] & u_ar['seco'] & u_solo['seco'], irrig['ligar'])
+rule11 = ctrl.Rule(temp['agradavel'] & u_ar['seco'] & u_solo['medio'], irrig['ligar'])
+rule12 = ctrl.Rule(temp['agradavel'] & u_ar['seco'] & u_solo['umido'], irrig['desligar'])
+
+
+rule13 = ctrl.Rule(temp['agradavel'] & u_ar['ideal'] & u_solo['seco'], irrig['ligar'])
+rule14 = ctrl.Rule(temp['agradavel'] & u_ar['ideal'] & u_solo['medio'], irrig['ligar'])
+rule15 = ctrl.Rule(temp['agradavel'] & u_ar['ideal'] & u_solo['umido'], irrig['desligar'])
+
+
+rule16 = ctrl.Rule(temp['agradavel'] & u_ar['umido'] & u_solo['seco'], irrig['ligar'])
+rule17 = ctrl.Rule(temp['agradavel'] & u_ar['umido'] & u_solo['medio'], irrig['ligar'])
+rule18 = ctrl.Rule(temp['agradavel'] & u_ar['umido'] & u_solo['umido'], irrig['desligar'])
+
+# Para temperatura (quente)
+rule19 = ctrl.Rule(temp['quente'] & u_ar['seco'] & u_solo['seco'], irrig['ligar'])
+rule20 = ctrl.Rule(temp['quente'] & u_ar['seco'] & u_solo['medio'], irrig['ligar'])
+rule21 = ctrl.Rule(temp['quente'] & u_ar['seco'] & u_solo['umido'], irrig['ligar'])
+
+
+rule22 = ctrl.Rule(temp['quente'] & u_ar['ideal'] & u_solo['seco'], irrig['ligar'])
+rule23 = ctrl.Rule(temp['quente'] & u_ar['ideal'] & u_solo['medio'], irrig['ligar'])
+rule24 = ctrl.Rule(temp['quente'] & u_ar['ideal'] & u_solo['umido'], irrig['desligar'])
+
+
+rule25 = ctrl.Rule(temp['quente'] & u_ar['umido'] & u_solo['seco'], irrig['ligar'])
+rule26 = ctrl.Rule(temp['quente'] & u_ar['umido'] & u_solo['medio'], irrig['ligar'])
+rule27 = ctrl.Rule(temp['quente'] & u_ar['umido'] & u_solo['umido'], irrig['desligar'])
+
+
+# Criando e simulando um controlador nebuloso
+sistema_controle = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10,
+rule11, rule12, rule13, rule14, rule15, rule16, rule17, rule18, rule19, rule20, rule21, rule22, rule23, rule24,
+rule25, rule26, rule27])
 sistema = ctrl.ControlSystemSimulation(sistema_controle)
 
-sistema.input['temp'] = 50
-sistema.input['umid'] = 66
+# Valores para teste
+sistema.input['temp'] = 33 # Quente
+sistema.input['u_solo'] = 600 # Medio
+sistema.input['u_ar'] = 58 # Mais ideal que seco 
+
+# Computando o resultado
 sistema.compute()
 print(sistema.output['irrig'])
 irrig.view(sim = sistema)
